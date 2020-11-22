ec52c76d15c6d14fa9176e728e28d5a4c39f5766
Alexander Fraltsov <kolonessens@yandex.ru>
2020-11-22 23:36:28 +0400

    fix: unnecessary sqs service and console logs deleted

--- src/product-service/db/create-product.ts
+++ src/product-service/db/create-product.ts
@@ -11,16 +11,12 @@ export const createProduct = async (product: IProduct, errorCb): Promise<IProduc
 
   try {
     await client.query('BEGIN');
-    console.log('BEGIN')
     const insertedProduct = await client.query(DML.INSERT_PRODUCTS.TEXT, [title, description, price]);
     id = insertedProduct.rows[0].id;
-    console.log(id)
     await client.query(DML.INSERT_STOCKS.TEXT, [id, count]);
     await client.query('COMMIT');
-    console.log('COMMIT')
   } catch (err) {
     await client.query('ROLLBACK');
-    console.log('ROLLBACK: ', err.message)
     errorCb(err);
   } finally {
     client.end();
